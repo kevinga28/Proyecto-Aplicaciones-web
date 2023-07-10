dbb81e81c5599a259f8b48b61d0342a343aa17ed
Ismael Leon <ileon30419@ufide.ac.cr>
2023-07-09 20:18:05 -0600

    Clases de Productos

---
+++ Proyecto/src/main/java/Proyecto/Proyecto/entities/Product.java
@@ -0,0 +1,77 @@
+package Proyecto.Proyecto.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "producto")
+public class Product {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private String ruta_imagen;
+    private double tamanio;
+    private String color;
+
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getRuta_imagen() {
+        return ruta_imagen;
+    }
+
+    public void setRuta_imagen(String ruta_imagen) {
+        this.ruta_imagen = ruta_imagen;
+    }
+
+    public double getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(double tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
